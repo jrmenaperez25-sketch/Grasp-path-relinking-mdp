5fbb390a96b822171f2bf55087e6188daa4c4023
jrmenaperez25-sketch <jrmenaperez25@gmail.com>
2025-12-10 21:33:50 +0100

    Actualizacion

--- Pycode GRASP for MDP (for students)/main.py
+++ Pycode GRASP for MDP (for students)/main.py
@@ -1,7 +1,12 @@
+import os
 from structure import instance
 from algorithms import grasp
 
+
 def executeInstance(instances, num_executions, output_file):
+    # Carpeta donde está este main.py
+    base_dir = os.path.dirname(__file__)
+
     with open(output_file, "w") as file:
         for path in instances:
             # Encabezado de instancia
@@ -10,14 +15,19 @@ def executeInstance(instances, num_executions, output_file):
             file.write("Ejecución | Mejor solución GRASP\n")
             file.write("--------------------------------------------------------\n")
 
-        for i in range(1, num_executions + 1):
-                inst = instance.readInstance(path)
-                sol = grasp.execute(inst, 10, 0.75)
+            for i in range(1, num_executions + 1):
+                # Ruta completa al fichero de instancia
+                full_path = os.path.join(base_dir, path)
 
-                # Guardamos el valor de la función objetivo
+                inst = instance.readInstance(full_path)
+                sol = grasp.execute(inst, 1, 0.75)
+
+                # Escribimos solo el valor de la función objetivo
                 file.write(f"{round(sol['of'], 2)} ")
 
-        file.write("\n========================================================\n\n")
+            # Línea en blanco + cierre de bloque
+            file.write("\n========================================================\n\n")
+
 
 if __name__ == '__main__':
     instances = [
@@ -37,7 +47,8 @@ if __name__ == '__main__':
         "instances/MDG-a_19_n500_m50.txt",
         "instances/MDG-a_20_n500_m50.txt"
     ]
-
-    num_executions = 2
+    
+    num_executions = 3
     output_file = "resultadosGRASP.txt"
     executeInstance(instances, num_executions, output_file)
+
